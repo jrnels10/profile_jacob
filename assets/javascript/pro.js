fc2b9429596ac05a27f5103ab5b5b4c8043e19ac
Jacob <jrnels10@gmail.com>
2018-07-14 15:30:40 -0700

    improved webpage

---
+++ assets/javascript/pro.js
@@ -0,0 +1,296 @@
+$(document).ready(function () {
+    $('.profile-name').hide();
+    $('.white-text').hide();
+    $('.intro-links').hide();
+    function nameTitle(value) {
+        setTimeout(function () {
+            $('.profile-name').fadeIn(2000);
+        }, value);
+    }
+
+    nameTitle(1000);
+    function slogan(value) {
+        setTimeout(function () {
+            $('.white-text').fadeIn(3000);
+            $('.intro-links').fadeIn(5000);
+        }, value);
+    }
+    slogan(2000);
+
+    let linkedIn = $(`<div class="row linky m-0"><svg
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:cc="http://creativecommons.org/ns#"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:svg="http://www.w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+    width="291"
+    height="79.459999"
+    id="svg3070"
+    version="1.1"
+    inkscape:version="0.48.0 r9654"
+    sodipodi:docname="New document 4">
+    <defs
+    id="defs3072" />
+    <sodipodi:namedview
+    id="base"
+    pagecolor="#ffffff"
+    bordercolor="#666666"
+    borderopacity="1.0"
+    inkscape:pageopacity="0.0"
+    inkscape:pageshadow="2"
+    inkscape:zoom="2.8"
+    inkscape:cx="142.93233"
+    inkscape:cy="33.703355"
+    inkscape:document-units="px"
+    inkscape:current-layer="layer1"
+    showgrid="false"
+    fit-margin-top="0"
+    fit-margin-left="0"
+    fit-margin-right="0"
+    fit-margin-bottom="0"
+    inkscape:window-width="1280"
+    inkscape:window-height="750"
+    inkscape:window-x="-8"
+    inkscape:window-y="-8"
+    inkscape:window-maximized="1" />
+    <metadata
+    id="metadata3075">
+    <rdf:RDF>
+    <cc:Work
+        rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+            rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+    </cc:Work>
+    </rdf:RDF>
+    </metadata>
+    <g
+    inkscape:label="Layer 1"
+    inkscape:groupmode="layer"
+    id="layer1"
+    transform="translate(-200.55198,-393.96227)">
+    <g
+        transform="matrix(1.018827,0,0,-1.018827,170.5996,498.03288)"
+        id="g3019">
+    <path
+        d="m 239.3298,95.036 c 0,2.96 2.4604,5.361 5.4956,5.361 l 63.376,0 c 3.0351,0 5.4956,-2.401 5.4956,-5.361 l 0,-64.117 c 0,-2.961 -2.4605,-5.361 -5.4956,-5.361 l -63.376,0 c -3.0352,0 -5.4956,2.4 -5.4956,5.361 l 0,64.117 z"
+        style="fill:#006699;fill-opacity:1;fill-rule:nonzero;stroke:none"
+        id="path14"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 31.1516,37.709 31.0356,0 0,10.189 -19.8042,0 0,38.874 -11.2314,0 0,-49.063 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path16"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 77.9001,37.709 0,33.793 -11.2324,0 0,-33.793 11.2324,0 z m -5.6162,38.407 c 3.917,0 6.355,2.595 6.355,5.84 -0.073,3.315 -2.438,5.837 -6.2803,5.837 -3.8428,0 -6.355,-2.522 -6.355,-5.837 0,-3.245 2.4375,-5.84 6.207,-5.84 l 0.073,0 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path18"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 83.1154,37.709 11.2325,0 0,18.872 c 0,1.01 0.073,2.019 0.3701,2.741 0.8115,2.017 2.6596,4.107 5.7627,4.107 4.0649,0 5.6909,-3.1 5.6909,-7.64 l 0,-18.08 11.2314,0 0,19.377 c 0,10.38 -5.542,15.21 -12.9321,15.21 -6.0591,0 -8.7197,-3.387 -10.1978,-5.694 l 0.075,0 0,4.9 -11.2325,0 c 0.1475,-3.171 0,-33.793 0,-33.793 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path20"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 133.2487,86.772 -11.2329,0 0,-49.063 11.2329,0 0,10.956 2.8072,3.534 8.7939,-14.49 13.8179,0 -14.7783,20.977 12.9316,14.272 -13.5225,0 c 0,0 -9.2368,-12.769 -10.0498,-14.283 l 0,28.097 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path22"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 188.7062,51.805 c 0.1475,0.868 0.3697,2.533 0.3697,4.415 0,8.736 -4.4346,17.604 -16.1094,17.604 -12.4888,0 -18.2524,-9.877 -18.2524,-18.835 0,-11.08 7.02,-18 19.2866,-18 4.8774,0 9.3843,0.72 13.0796,2.234 l -1.4785,7.418 c -3.0293,-1.005 -6.1329,-1.507 -9.9751,-1.507 -5.2466,0 -9.8277,2.153 -10.1978,6.743 l 23.2773,-0.07 z m -23.3505,7.599 c 0.2954,2.884 2.2168,7.138 7.02,7.138 5.0976,0 6.2807,-4.543 6.2807,-7.138 l -13.3007,0 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path24"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 216.801,86.772 0,-16.984 -0.1475,0 c -1.626,2.377 -5.0259,3.963 -9.5322,3.963 -8.6465,0 -16.2573,-6.92 -16.1841,-18.741 0,-10.958 6.8726,-18.094 15.4443,-18.094 4.6553,0 9.0889,2.019 11.3057,5.912 l 0.2222,0 0.4433,-5.119 9.9766,0 c -0.148,2.379 -0.2969,6.488 -0.2969,10.524 l 0,38.539 -11.2314,0 z m 0,-33.421 c 0,-0.864 -0.074,-1.73 -0.2222,-2.45 -0.6641,-3.1 -3.3252,-5.262 -6.5757,-5.262 -4.6557,0 -7.6855,3.748 -7.6855,9.659 0,5.55 2.5869,10.019 7.7588,10.019 3.4726,0 5.9116,-2.378 6.5771,-5.333 0.1475,-0.65 0.1475,-1.371 0.1475,-2.019 l 0,-4.614 z"
+        style="fill:#000000;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path26"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 261.8728,37.749 0,33.794 -11.2325,0 0,-33.794 11.2325,0 z m -5.6163,38.408 c 3.917,0 6.355,2.595 6.355,5.838 -0.073,3.316 -2.438,5.839 -6.2807,5.839 -3.8423,0 -6.3545,-2.523 -6.3545,-5.839 0,-3.243 2.4375,-5.838 6.207,-5.838 l 0.073,0 z"
+        style="fill:#ffffff;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path28"
+        inkscape:connector-curvature="0" />
+    <path
+        d="m 268.0881,37.749 11.2324,0 0,18.872 c 0,1.01 0.073,2.019 0.3696,2.741 0.812,2.018 2.6602,4.108 5.7632,4.108 4.0645,0 5.6904,-3.099 5.6904,-7.642 l 0,-18.079 11.2315,0 0,19.377 c 0,10.38 -5.5415,15.21 -12.9316,15.21 -6.0596,0 -8.7198,-3.387 -10.1978,-5.694 l 0.075,0 0,4.901 -11.2324,0 c 0.1474,-3.171 0,-33.794 0,-33.794 z"
+        style="fill:#ffffff;fill-opacity:1;fill-rule:evenodd;stroke:none"
+        id="path30"
+        inkscape:connector-curvature="0" />
+    </g>
+    </g>
+    </svg></div>`);
+    let gitHub = $(`<div class="row gitty m-0"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+    width="212px" height="212px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+   <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M408.027,408.027
+       c-19.76,19.759-42.756,35.267-68.354,46.094c-6.502,2.75-13.105,5.164-19.801,7.246V423c0-20.167-6.916-35-20.75-44.5
+       c8.668-0.833,16.625-2,23.875-3.5s14.918-3.667,23-6.5c8.084-2.833,15.334-6.208,21.75-10.125c6.418-3.917,12.584-9,18.5-15.25
+       c5.918-6.25,10.875-13.333,14.875-21.25s7.168-17.417,9.5-28.5c2.334-11.083,3.5-23.292,3.5-36.625c0-25.833-8.416-47.833-25.25-66
+       c7.668-20,6.834-41.75-2.5-65.25l-6.25-0.75c-4.332-0.5-12.125,1.333-23.375,5.5s-23.875,11-37.875,20.5
+       c-19.832-5.5-40.416-8.25-61.749-8.25c-21.5,0-42,2.75-61.5,8.25c-8.833-6-17.208-10.958-25.125-14.875s-14.25-6.583-19-8
+       s-9.167-2.292-13.25-2.625s-6.708-0.417-7.875-0.25s-2,0.333-2.5,0.5c-9.333,23.667-10.167,45.417-2.5,65.25
+       c-16.833,18.167-25.25,40.167-25.25,66c0,13.333,1.167,25.542,3.5,36.625s5.5,20.583,9.5,28.5s8.958,15,14.875,21.25
+       s12.083,11.333,18.5,15.25s13.667,7.292,21.75,10.125s15.75,5,23,6.5s15.208,2.667,23.875,3.5c-13.667,9.333-20.5,24.167-20.5,44.5
+       v39.115c-7.549-2.247-14.99-4.902-22.3-7.994c-25.597-10.827-48.594-26.335-68.353-46.094
+       c-19.758-19.758-35.267-42.756-46.093-68.354C46.679,313.195,41,285.043,41,256s5.679-57.195,16.879-83.675
+       c10.827-25.597,26.335-48.594,46.093-68.353c19.758-19.759,42.756-35.267,68.353-46.093C198.805,46.679,226.957,41,256,41
+       s57.195,5.679,83.676,16.879c25.598,10.827,48.594,26.335,68.354,46.093c19.758,19.758,35.266,42.756,46.092,68.353
+       C465.32,198.805,471,226.957,471,256s-5.68,57.195-16.879,83.675C443.295,365.271,427.785,388.27,408.027,408.027z"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg></div>`);
+
+    $('.intro-links').append(linkedIn,gitHub)
+
+
+
+
+
+
+    let projectPort = [
+        mapUp = {
+            num: '1',
+            link: 'https://xeroneon.github.io/Project-1/',
+            image: 'assets/images/map-up.PNG',
+            label: 'Map-Up',
+            preview: 'https://www.youtube.com/embed/6ZW3xXFesLY?autoplay=1',
+        },
+        trivia = {
+            num: '2',
+            link: 'https://jrnels10.github.io/Trivia-game/',
+            image: 'assets/images/train.jpeg',
+            label: 'Cowboy Trivia',
+            descr: 'timed train stuff',
+
+        },
+        gifTastic = {
+            num: '3',
+            link: 'https://jrnels10.github.io/GifTastic/',
+            image: 'https://media3.giphy.com/media/11ISwbgCxEzMyY/giphy.gif',
+            label: 'GifTastic!',
+            descr: 'timed train stuff',
+
+        },
+        crazyTrain = {
+            num: '4',
+            link: 'https://jrnels10.github.io/Crazy_Train/',
+            image: 'assets/images/wild_west.jpeg',
+            label: 'Crazy Train',
+            descr: 'timed train stuff',
+        }
+
+    ]
+
+    console.log(projectPort.length)
+
+    let box;
+
+    // $('.portfolio').on('click', function projDiv() {
+    //     console.log('port click');
+    //     $('.change').replaceWith('<div class="row port-stuff">');
+    function loadCarousel() {
+
+        for (i = 0; i < projectPort.length; i++) {
+            console.log(projectPort[i]);
+            box = $(`
+            <div class="col-lg-4 col-md-6 mb-4">
+
+            <div class="view overlay z-depth-1-half h-100" id="images">
+                <img src="${projectPort[i].image}" class="img-fluid"  >
+                <a href="${projectPort[i].link}">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <h4 class="my-4 font-weight-bold" id='${projectPort[i].label}'></h4>
+            <p class="grey-text" id='${projectPort[i].descr}></p>
+
+        </div>
+                        
+                  
+                `);
+            $('#test-p').append(box);
+        }
+    };
+    loadCarousel();
+    var target;
+    $('.sample').on('click', function () {
+        console.log('clicked preview')
+
+
+
+        target = $(this).attr('id');
+        for (i = 0; i < projectPort.length; i++) {
+            if (target === projectPort[i].num) {
+                console.log('I found ya!');
+                // $('#projectPort[i].name').css('margin-left', '10px');
+                let showWindow = $(`<div class='row vid m-auto' style='width: 540px; height: 390px;>
+                                        <div class= 'col-12>
+                                            <div class='row btn btn-secondary close-bar' style='height: 40px; background: whitesmoke'>
+                                                <button class='close'>Close window</button>
+                                            </div>
+                                            <div class='row video-section'>
+                                                <iframe width="540" height="340" src="${projectPort[i].preview}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `)
+                $('.video-preview').append(showWindow);
+            };
+        }
+
+
+        $('.close').on('click', function () {
+            console.log('target');
+            $('.video-preview').empty();
+
+
+            loadCarousel()
+        });
+    });
+
+
+
+
+
+
+});
+
+
+new WOW().init();
+
+
+
