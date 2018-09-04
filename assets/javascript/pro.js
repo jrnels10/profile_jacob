755b23fded13ee1e309370fb517cbb649f5235df
Jacob <jrnels10@gmail.com>
2018-09-04 16:04:32 -0700

    fixed project layout

--- assets/javascript/pro.js
+++ assets/javascript/pro.js
@@ -184,7 +184,7 @@ $(document).ready(function () {
         mapUp = {
             num: '1',
             link: 'https://xeroneon.github.io/Project-1/',
-            image: 'assets/images/map-up.PNG',
+            image: 'assets/images/mapup.jpeg',
             label: 'Map-Up',
             preview: 'https://www.youtube.com/embed/6ZW3xXFesLY?autoplay=1',
         },
@@ -217,7 +217,14 @@ $(document).ready(function () {
             image: 'assets/images/friend_finder.png',
             label: 'Friend Finder',
             descr: 'timed train stuff',
-        }
+        },
+        // travelBuddies = {
+        //     num: '6',
+        //     link: 'https://polar-crag-61522.herokuapp.com/',
+        //     image: 'assets/images/friend_finder.png',
+        //     label: 'Friend Finder',
+        //     descr: 'timed train stuff',
+        // }
     ]
 
     console.log(projectPort.length)
@@ -232,59 +239,26 @@ $(document).ready(function () {
         for (i = 0; i < projectPort.length; i++) {
             console.log(projectPort[i]);
             box = $(`
-            <div class="col-lg-4 col-md-6 proj-cont" id="${projectPort[i].num}">
+            <div class="col-lg-4 col-md-6 proj-cont position-relative" id="${projectPort[i].num}">
 
-            <div class="view overlay z-depth-1-half h-100" id="images">
+                <div class="view z-depth-1-half h-100" id="images">
                 <img src="${projectPort[i].image}" class="img-fluid">
+                <h4 class="centered my-4 font-weight-bold">${projectPort[i].label}</h4>
                 <a href="${projectPort[i].link}">
                     <div class="mask rgba-white-slight"></div>
                 </a>
                 </div>
                 
-                <h4 class="my-4 font-weight-bold" id='${projectPort[i].label}'></h4>
                 <p class="grey-text" id='${projectPort[i].descr}></p>
                 
-                </div>
+            </div>
                 
                 
                 `);
             $('#test-p').append(box);
         }
     };
-    // <button class="btn primary-btn sample w-100 mb-4">Preview</button>
     loadCarousel();
-    // var target;
-    // $('.proj-con').hover(function() {
-    //     console.log('clicked preview')
-
-    //     target = $(this).attr('id');
-    //     console.log(target)
-    //     for (i = 0; i < projectPort.length; i++) {
-    //         if (target === projectPort[i].num) {
-    //             console.log('I found ya!');
-    //             // $('#projectPort[i].name').css('margin-left', '10px');
-    //             let showWindow = $(`
-    //                                         <div class='row video-section'>
-    //                                             <iframe width="540" height="340" src="${projectPort[i].preview}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    //                                         </div>
-
-    //                                 `)
-    //             $('.prj-img').replaceWidth(showWindow);
-    //         };
-    //     }
-
-
-    //     $('.close').on('click', function () {
-    //         console.log('target');
-    //         $('.video-preview').empty();
-
-
-    //         loadCarousel()
-    //     });
-    // });
-
-
-
 
 
 
